fb2f6b0ef1a206c0b43cbc5353ebaf54e9b265ea
WuCiel <871447603@qq.com>
2017-11-29 23:30:08 +0800

    可以拍照并且储存到SD卡根目录的文件夹aaa

--- app/src/main/java/com/example/cier/cameratest/Camera2Activity.java
+++ app/src/main/java/com/example/cier/cameratest/Camera2Activity.java
@@ -14,10 +14,13 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.media.Image;
 import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.PersistableBundle;
@@ -36,8 +39,14 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.List;
 import java.util.logging.LogRecord;
 
 /**
@@ -45,7 +54,7 @@ import java.util.logging.LogRecord;
  */
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-public class Camera2Activity extends AppCompatActivity implements View.OnClickListener {
+public class Camera2Activity extends AppCompatActivity implements View.OnClickListener,View.OnLongClickListener {
 
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
     private static final int REQUEST_CODE = 0x001;
@@ -70,6 +79,11 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
     private CameraCaptureSession mCameraCaptureSession;
     private CameraDevice mCameraDevice;
 
+    private static String savePath;
+    private static int photoIndex = 1;
+
+    private static int photoAmount=10;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,11 +98,18 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
      * 初始化
      */
     private void initView() {
+        savePath=Environment.getExternalStorageDirectory()+"/aaa";
+        File dir=new File(Environment.getExternalStorageDirectory(),"aaa");
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+
         show = (ImageView) findViewById(R.id.show);
 
         //mSurfaceView
         mSurfaceView = (SurfaceView) findViewById(R.id.surfaceView);
         mSurfaceView.setOnClickListener(this);
+        mSurfaceView.setOnLongClickListener(this);
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.setKeepScreenOn(true);
 
@@ -144,7 +165,8 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
              */
             @Override
             public void onImageAvailable(ImageReader reader) {
-                mCameraDevice.close();
+                Log.i(TAG, "capture a photo");
+                /*mCameraDevice.close();
                 mSurfaceView.setVisibility(View.GONE);
                 show.setVisibility(View.VISIBLE);
                 //获取照片数据
@@ -155,7 +177,10 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
                 final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 if (bitmap != null) {
                     show.setImageBitmap(bitmap);
-                }
+                }*/
+                File mFile=new File(savePath,"pic"+(photoIndex++)+".jpeg");
+                mainhandler.post(new ImageSaver(reader.acquireNextImage(),mFile));
+
             }
         }, mainhandler);
 
@@ -163,15 +188,16 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             try {
-                Log.i(TAG,"turning on camera");
+                Log.i(TAG, "turning on camera");
                 //使用摄像头管理器打开摄像头
+                //打开后会回调mStateCallback对象，也就是下方的CameraDevice.StateCallback里的onOpened()函数
                 mCameraManager.openCamera(mCameraID, mStateCallback, mainhandler);
             } catch (CameraAccessException e) {
                 e.printStackTrace();
             }
         } else {
             checkPermission();
-            Log.e(TAG,"no camera permission");
+            Log.e(TAG, "no camera permission");
             Toast.makeText(this, "no camera permission", Toast.LENGTH_SHORT).show();
         }
     }
@@ -181,7 +207,7 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
      */
     private CameraDevice.StateCallback mStateCallback = new CameraDevice.StateCallback() {
         /**
-         * 打开摄像头
+         * 打开摄像头时调用
          */
         @Override
         public void onOpened(CameraDevice camera) {//打开摄像头
@@ -191,7 +217,7 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
         }
 
         /**
-         * 关闭摄像头
+         * 关闭摄像头时调用
          */
         @Override
         public void onDisconnected(CameraDevice camera) {
@@ -217,16 +243,18 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
             //将SurfaceView的Surface作为CaptureRequest.Builder的目标
             previewRequestBuilder.addTarget(mSurfaceHolder.getSurface());
             //创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            //createCaptureSession()方法会回调CameraCaptureSession.StateCallback()，所以重写里面的
+            // onConfigured()就可以预览拍照页面
             mCameraDevice.createCaptureSession(Arrays.asList(mSurfaceHolder.getSurface(),
                     mImageReader.getSurface()), new CameraCaptureSession.StateCallback() {
 
                 /**
-                 * 摄像头已经准备好
+                 * 摄像头已经准备好，可以开始预览了
                  */
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
                     if (mCameraDevice == null) {
-                        Log.e(TAG,"mCameraDevice = null");
+                        Log.e(TAG, "mCameraDevice = null");
                         return;
                     }
 
@@ -240,8 +268,21 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
                         previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
                                 CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
                         //显示预览
+                        //调用setRepeatingRequest()就开始预览了
+                        //setRepeatingRequest()会调用CameraCaptureSession.CaptureCallback
+                        //重写CameraCaptureSession.CaptureCallback中的
+                        //onCaptureCompleted方法，result就是未经过处理的元数据了
                         CaptureRequest previewRequest = previewRequestBuilder.build();
-                        mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
+                        //
+                        CameraCaptureSession.CaptureCallback mCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+                            @Override
+                            public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+                                Log.i(TAG,"mCaptureCallback -> onCaptureCompleted");
+                                
+                            }
+                        };
+                        //
+                        mCameraCaptureSession.setRepeatingRequest(previewRequest, mCaptureCallback, childHandler);
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     }
@@ -293,6 +334,10 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE);
         }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS}, REQUEST_CODE);
+        }
     }
 
     @Override
@@ -306,13 +351,17 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
             }
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                 try {
-                    Log.i(TAG,"turning on camera");
+                    Log.i(TAG, "turning on camera");
                     mCameraManager.openCamera(mCameraID, mStateCallback, mainhandler);
                 } catch (CameraAccessException e) {
                     e.printStackTrace();
                 }
             }
             Toast.makeText(this, "get permission success", Toast.LENGTH_SHORT).show();
+            File dir=new File(Environment.getExternalStorageDirectory(),"aaa");
+            if(!dir.exists()){
+                dir.mkdirs();
+            }
         }
     }
 
@@ -320,5 +369,55 @@ public class Camera2Activity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         takePicture();
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        Log.v(TAG,"long click ---> surfaceView");
+        return false;
+    }
+
+    /**
+     * Saves a JPEG {@link Image} into the specified {@link File}.
+     */
+    private static class ImageSaver implements Runnable {
+
+        /**
+         * The JPEG image
+         */
+        private final Image mImage;
+        /**
+         * The file we save the image into.
+         */
+        private final File mFile;
+
+        ImageSaver(Image image, File file) {
+            mImage = image;
+            mFile = file;
+        }
+
+        @Override
+        public void run() {
+            ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.remaining()];
+            buffer.get(bytes);
+            FileOutputStream output = null;
+            try {
+                output = new FileOutputStream(mFile);
+                output.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                mImage.close();
+                if (null != output) {
+                    try {
+                        output.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
     }
 }
